41ca3925559aa29ac42d4421eff01076368a4de4
Kurumina <ana.marcacini2000@gmail.com>
2022-05-26 12:06:25 -0300

    Inicio projeto SO

---
+++ Sistemas Operacionais/Threads em Java/Ana/ProducerConsumer/src/prodcon/Account.java
@@ -0,0 +1,18 @@
+package prodcon;
+
+public class Account {
+    private float balance; //saldo
+
+    protected Cliente c1;
+    protected Cliente c2;
+    
+
+
+
+}
+
+//realizar um depósito ou uma retirada
+//(jogue uma moeda antes de executar e escolha ...).
+
+
+//O banco não aceita que retiradas sejam executadas quando a conta não possui saldo sufi- ciente (não se admite saldo negativo). Os clientes podem continuamente realizar depósitos ou retiradas e cabe ao seu programa sincronizar como isso será feito, de modo a manter a consistência e integridade da conta-corrente
---
+++ Sistemas Operacionais/Threads em Java/Ana/ProducerConsumer/src/prodcon/Cliente.java
@@ -0,0 +1,20 @@
+package prodcon;
+
+public class Cliente {
+    private String nome;
+    private Account conta;
+
+
+    public Cliente(String nome, Account conta) {
+        this.nome = nome;
+        this.conta = conta;
+    }
+
+    private int execute(){
+        
+    }
+
+
+
+    
+}
